e17b78935aeb52408be8b851a6e445feee06989b
gjampanih <GirishJampaniHanumantha@iHeartMedia.com>
2022-07-05 16:20:14 -0500

    adds-hackathon-22:initial push to remote

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experimenting with different audio data representations and anomaly detection algorithms for 2022 Hackathon',
+    author='Girish Jampani',
+    license='MIT',
+)
